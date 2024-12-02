cda3b07fae0b5968438d1dc1ee82736c58a70ecf
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-12-03 01:30:27 +0900

    add: Java Module 추가(Kafka 기본)

---
+++ java-kafka-first/README.md
@@ -0,0 +1,9 @@
+## Kafka Basic Test Module
+
+### Info
+- Java Project(Not Spring Boot)
+- Java 17
+- Kafka 3.1.0
+- Slf4j
+
+* Java - Kafka 기본기능 테스트
---
+++ java-kafka-first/src/main/java/com/lucas/javaKafkaFirst/ProducerDemo.java
@@ -0,0 +1,7 @@
+package com.lucas.javaKafkaFirst;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
