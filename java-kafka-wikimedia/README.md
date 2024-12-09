a1b6a0716d7447cf5da3a52c6859a9667e09243f
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-12-09 17:15:17 +0900

    add: Java Module - Kafka Wikimedia

---
+++ java-kafka-wikimedia/README.md
@@ -0,0 +1,3 @@
+## Java Project (Kafka From Wikimedia)
+
+- 자바프로젝트: 위키미디어 데이터를 Kafka 로 가져와서 처리하는 프로젝트
---
+++ java-kafka-wikimedia/src/main/java/com/lucas/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,10 @@
+package com.lucas.kafka.wikimedia;
+
+/**
+ * Wikimedia Kafka Producer Main
+ */
+public class WikimediaChangesProducer {
+    public static void main(String[] args) {
+
+    }
+}
