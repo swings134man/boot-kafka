f0b4dd6c2d10c1e1f2c347ec2233c54bcbd224a0
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-12-12 18:08:01 +0900

    feature: Kafka Producer - MSG 압축 기능 & docs

--- java-kafka-wikimedia/src/main/java/com/lucas/kafka/wikimedia/WikimediaChangesProducer.java
+++ java-kafka-wikimedia/src/main/java/com/lucas/kafka/wikimedia/WikimediaChangesProducer.java
@@ -30,6 +30,11 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // -1
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE)); // -1
+
+        // Batch Config & Compression
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); // 20ms 대기
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); // 32KB
     }
 
     // Event Handler
