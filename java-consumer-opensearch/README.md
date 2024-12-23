c75f17b70866742b75d99a8fd02ab348f3f13a28
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-12-23 23:31:20 +0900

    add: Java-consumer-opensearch module init

---
+++ java-consumer-opensearch/README.md
@@ -0,0 +1,4 @@
+## Java-kafka-consumer-opensearch 
+- Java Module
+- Kafka Consumer
+- To opensearch(docker)
---
+++ java-consumer-opensearch/src/main/java/com/test/OpenSearchConsumer.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @package : com.test
+ * @name : OpenSearchConsumer.java
+ * @date : 2024. 12. 23. 오후 11:30
+ * @author : lucaskang(swings134man)
+ * @Description: OpenSearchConsumer Kafka
+**/
+public class OpenSearchConsumer {
+    public static void main(String[] args) {
+
+    }
+}
