b77f4dea2889f3178e833d8a088f00f8eefab094
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2025-07-25 15:51:28 +0900

    fix: Kafka Model ID 채번 (UUID)

--- flux-kafka/src/main/kotlin/com/lucas/fluxkafka/commons/message/KafkaMessageDTO.kt
+++ flux-kafka/src/main/kotlin/com/lucas/fluxkafka/commons/message/KafkaMessageDTO.kt
@@ -1,11 +1,12 @@
 package com.lucas.fluxkafka.commons.message
 
 import java.time.LocalDateTime
+import java.util.UUID
 
 
 data class KafkaMessageDTO(
     val topic: String = "",
-    val id: String = "",
+    val id: String = UUID.randomUUID().toString(),
     val sender: String = "",
     val message: Any?,
     val timeStamp: LocalDateTime = LocalDateTime.now()
